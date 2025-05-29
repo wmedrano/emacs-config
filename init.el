8650d5382846cd980de18f73a2f411964051f10a
Will Medrano <will@wmedrano.dev>
2025-05-29 07:16:55 -0700

    Add yaml and markdown support.

--- init.el
+++ init.el
@@ -7,9 +7,12 @@
                 ivy
                 counsel
                 company
-                rust-mode
                 htmlize
-                magit))
+                magit
+                rust-mode
+                yaml-mode
+                markdown-mode
+                ))
 
 (package-initialize)
 
@@ -69,15 +72,3 @@
 (add-hook 'before-save-hook #'eglot-maybe-format-buffer)
 
 (global-company-mode t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(company counsel doom-themes htmlize ivy parrot rust-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
