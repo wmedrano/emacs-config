25c0773634721056530f44d08bc2fd35b19fa308
Will Medrano <will@wmedrano.dev>
2025-05-29 07:14:54 -0700

    Add ibuffer.

--- init.el
+++ init.el
@@ -46,6 +46,8 @@
 
 (define-key counsel-mode-map (kbd "C-x b") #'counsel-switch-buffer)
 
+(global-set-key (kbd "C-x C-b") #'ibuffer)
+
 (add-hook 'rust-mode-hook #'eglot-ensure)
 
 (setq-default indent-tabs-mode nil)
@@ -67,3 +69,15 @@
 (add-hook 'before-save-hook #'eglot-maybe-format-buffer)
 
 (global-company-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(company counsel doom-themes htmlize ivy parrot rust-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
