38321be31f49b0c3459de5647d435095eac437ab
Will Medrano <will@wmedrano.dev>
2026-06-18 23:20:32 -0700

    Add agent shell config

--- init.el
+++ init.el
@@ -8,14 +8,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ace-window anzu auto-highlight-symbol clang-format company consult diff-hl
-                doom-modeline doom-themes dracula-theme eglot eldoc embark
-                embark-consult erc evil evil-terminal-cursor-changer flycheck
-                flymake gn-mode gnuplot gptel htmlize lua-mode marginalia
-                markdown-mode orderless org package-lint peg posframe python rg
-                rust-mode smartparens tramp transient ttx-mode typescript-mode
-                vertico vertico-posframe vundo which-key yaml-mode zig-mode
-                zig-ts-mode))
+   '(ace-window agent-shell anzu auto-highlight-symbol clang-format company consult
+                diff-hl doom-modeline doom-themes dracula-theme eglot eldoc
+                embark embark-consult erc evil evil-terminal-cursor-changer
+                flycheck flymake gn-mode gnuplot gptel htmlize lua-mode
+                marginalia markdown-mode orderless org package-lint peg posframe
+                python rg rust-mode smartparens tramp transient ttx-mode
+                typescript-mode vertico vertico-posframe vundo which-key
+                yaml-mode zig-mode zig-ts-mode))
  '(safe-local-variable-values
    '((eval and buffer-file-name (not (eq major-mode 'package-recipe-mode))
            (or (require 'package-recipe-mode nil t)
@@ -50,6 +50,7 @@
 (require 'ace-window)
 (when (display-graphic-p)
   (require 'ace-window-posframe))
+(require 'agent-shell)
 (require 'ansi-color)
 (require 'ansi-osc)
 (require 'anzu)
@@ -498,6 +499,11 @@ def add_numbers(a: int, b: int) -> int:
  gptel-default-mode 'org-mode)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Agent Shell
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq-default agent-shell-preferred-agent-config 'opencode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Evil Mode
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq-default evil-cross-lines t
@@ -534,6 +540,23 @@ def add_numbers(a: int, b: int) -> int:
 (defvar leader-map (make-sparse-keymap) "Leader key keymap.")
 (define-key evil-motion-state-map (kbd "SPC") leader-map)
 
+;; SPC a
+(define-key leader-map "aa" #'agent-shell-send-dwim)
+(define-key leader-map "an" #'agent-shell-new-shell)
+(define-key leader-map "at" #'agent-shell-new-temp-shell)
+
+;; SPC e
+(define-key leader-map "ea" #'eglot-code-actions)
+(define-key leader-map "ee" #'consult-flymake)
+(define-key leader-map "ef" #'flymake-eglot-fix-all)
+(define-key leader-map "ei" #'eglot-inlay-hints-mode)
+(define-key leader-map "er" #'eglot-rename)
+
+;; SPC h
+(define-key leader-map "he" #'eldoc)
+(define-key leader-map "hh" #'highlight-symbol-at-point)
+(define-key leader-map "hk" #'unhighlight-regexp)
+
 ;; SPC s
 (define-key leader-map "ss" #'consult-ripgrep)
 (define-key leader-map "sr" #'rg)
@@ -543,13 +566,6 @@ def add_numbers(a: int, b: int) -> int:
 (define-key leader-map "si" #'consult-imenu)
 (define-key leader-map "sI" #'consult-imenu-multi)
 (define-key leader-map "sf" #'consult-flymake)
-
-;; SPC e
-(define-key leader-map "ea" #'eglot-code-actions)
-(define-key leader-map "ee" #'consult-flymake)
-(define-key leader-map "ef" #'flymake-eglot-fix-all)
-(define-key leader-map "ei" #'eglot-inlay-hints-mode)
-(define-key leader-map "er" #'eglot-rename)
 
 ;; Ace Window
 (setq-default aw-dispatch-always t)
