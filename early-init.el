87d6468f9bd33523e6ac43d79def1ec92ddcbd1b
wmedrano <wmedrano@chromium.org>
2026-08-24 14:56:53 -0700

    Do less redraws on init

---
+++ early-init.el
@@ -0,0 +1,7 @@
+;; -*- lexical-binding: t; -*-
+
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars . nil) default-frame-alist)
+(push '(horizontal-scroll-bars . nil) default-frame-alist)
+(push '(fullscreen . maximized) default-frame-alist)
